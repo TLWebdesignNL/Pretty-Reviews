5cc777a5bafd1a20c56a546c986ff643faed0c5d
TLWebdesign <info@tlwebdesign.nl>
2026-06-23 18:25:44 +0200

    Update compact.php

--- tmpl/compact.php
+++ tmpl/compact.php
@@ -150,7 +150,6 @@ $writeReviewUrl    = $safeUrl($writeReviewUrl ?? '');
                                             <?php endif; ?>
                                         </div>
                                     </div>
-                                </div>
                     </div>
                 <?php endforeach; ?>
         </div>
